0d6edde6aa93fa9bfb6c7b4312e08f3ef544366c
Marelso <klein.marcelojr@gmail.com>
2024-03-04 05:35:49 -0300

    Updating attendant data model to has the necessary database fields

--- composeApp/src/desktopMain/kotlin/data/Attendant.kt
+++ composeApp/src/desktopMain/kotlin/data/Attendant.kt
@@ -1,8 +1,23 @@
 package data
 
-data class Attendant(
-    val id: Int,
-    val name: String,
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "attendant")
+class Attendant(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "bio")
     val bio: String,
+
+    @ColumnInfo(name = "link")
     val link: String
 )
