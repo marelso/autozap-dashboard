90e756e36046af69cb9cbf5746b4e1bed127bff5
Marelso <klein.marcelojr@gmail.com>
2024-03-04 05:59:34 -0300

    Removing unused imports

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,8 +1,5 @@
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import org.koin.core.KoinApplication
-import org.koin.core.context.KoinContext
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "Autozap dashboard") {
