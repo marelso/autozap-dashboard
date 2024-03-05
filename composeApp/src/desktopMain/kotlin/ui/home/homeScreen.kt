f9aba127485cd1332808bfdc1789b114141c5a50
Marelso <klein.marcelojr@gmail.com>
2024-03-05 07:05:21 -0300

    Updating include icon

--- composeApp/src/desktopMain/kotlin/ui/home/homeScreen.kt
+++ composeApp/src/desktopMain/kotlin/ui/home/homeScreen.kt
@@ -2,12 +2,12 @@ package ui.home
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -54,18 +54,13 @@ fun homeScreen(
                         border = BorderStroke(
                             width = 2.dp, color = MaterialTheme.colors.primary
                         ), shape = RoundedCornerShape(10.dp)
-                    ), contentAlignment = Alignment.Center
+                    ).clickable { onCreateClick() }, contentAlignment = Alignment.Center
                 ) {
-                    IconButton(
-                        content = {
-                            Icon(
-                                modifier = Modifier.size(40.dp),
-                                imageVector = Icons.Default.Add,
-                                tint = MaterialTheme.colors.primary,
-                                contentDescription = "Include"
-                            )
-                        },
-                        onClick = { onCreateClick() }
+                    Icon(
+                        modifier = Modifier.size(40.dp),
+                        imageVector = Icons.Default.Add,
+                        tint = MaterialTheme.colors.primary,
+                        contentDescription = "Include new attendant"
                     )
                 }
             }
