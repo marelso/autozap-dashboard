c719be9452014960e3b3667f49c939a3dc2f1f42
Marelso <klein.marcelojr@gmail.com>
2024-03-08 07:42:42 -0300

    Optimizing imports

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,4 +1,3 @@
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
--- composeApp/src/desktopMain/kotlin/ui/home/homeScreen.kt
+++ composeApp/src/desktopMain/kotlin/ui/home/homeScreen.kt
@@ -1,18 +1,16 @@
 package ui.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
