20a2b1981fe7347b10b1ffce66e06753834b37a3
Marelso <klein.marcelojr@gmail.com>
2024-03-08 02:30:00 -0300

    Updating auth

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -1,6 +1,8 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
@@ -11,6 +13,7 @@ import data.WindowsServiceManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.home.*
+import java.io.File
 
 @Composable
 @Preview
@@ -25,6 +28,52 @@ fun App() {
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             val attendantDao = DesktopDatabase.getInstance().getAttendantDao()
             val attendants = MutableStateFlow(attendantDao.fetch())
+
+            Button(onClick = {
+                openAuth()
+
+//                val command = " node ~/Documents/projects/marelso/autozap/auth.js ; read -p 'Press Enter to exit...'" // Command to run Node.js script
+//                val os = System.getProperty("os.name").lowercase(Locale.getDefault())
+//                val console = when {
+//                    os.contains("win") -> "cmd.exe" // For Windows
+//                    os.contains("mac") -> "Terminal" // For macOS
+//                    os.contains("nix") || os.contains("nux") || os.contains("aix") -> "gnome-terminal" // For Linux/Unix
+//                    else -> {
+//                        println("Unsupported operating system")
+//                        return@Button
+//                    }
+//                }
+//
+//                try {
+//                    val processBuilder = ProcessBuilder("gnome-terminal", "--", "sdk current; sleep 10")
+//                    processBuilder.directory(File(System.getProperty("user.home")))
+//                    processBuilder.start()
+//                } catch (e: Exception) {
+//                    println("Error starting terminal: ${e.message}")
+//                }
+
+
+//                val os = System.getProperty("os.name").lowercase(Locale.getDefault())
+//                val console = when {
+//                    os.contains("win") -> "cmd.exe" // For Windows
+//                    os.contains("mac") -> "Terminal" // For macOS
+//                    os.contains("nix") || os.contains("nux") || os.contains("aix") -> "gnome-terminal" // For Linux/Unix
+//                    else -> {
+//                        println("Unsupported operating system")
+//                        return@Button
+//                    }
+//                }
+//                val command = "ls ; read -p 'Press Enter to exit...'"
+//                try {
+//                    val processBuilder = ProcessBuilder(console, "-c", command)
+//                    processBuilder.directory(File(System.getProperty("user.dir")))
+//                    processBuilder.start().waitFor()
+//                } catch (e: Exception) {
+//                    println("Error starting terminal: ${e.message}")
+//                }
+            }) {
+                Text("Click")
+            }
 
             homeScreen(
                 attendants = attendants.value.value,
@@ -84,4 +133,17 @@ fun App() {
             }
         }
     }
+}
+
+private fun openAuth() {
+    val command = listOf(
+        "gnome-terminal",
+        "--",
+        "sh",
+        "-c",
+        "./auth.sh"
+    )
+    val processBuilder = ProcessBuilder(command).directory(File(System.getProperty("user.dir")))
+
+    processBuilder.start()
 }
