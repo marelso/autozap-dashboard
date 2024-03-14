9912a72652595acb6cddc8dfac9b3eb03a0f4102
Marcelo <klein.marcelojr@gmail.com>
2024-03-14 02:53:55 -0300

    Created new datamodel to handle replies

---
+++ composeApp/src/desktopMain/kotlin/data/reply/Reply.kt
@@ -0,0 +1,20 @@
+package data.reply
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "reply")
+class Message(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "reply")
+    var message: String,
+
+    @ColumnInfo(name = "active")
+    var active: Boolean
+)
