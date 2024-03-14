c302a0d7bab0616b9b28ab53f3b821a2ad6888dc
Marcelo <klein.marcelojr@gmail.com>
2024-03-14 00:37:22 -0300

    Including new callback to handle message exclusion

--- composeApp/src/desktopMain/kotlin/ui/home/messageListComponent.kt
+++ composeApp/src/desktopMain/kotlin/ui/home/messageListComponent.kt
@@ -1,14 +1,15 @@
 package ui.home
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,16 +23,22 @@ fun messageListComponent(
     current: Message?,
     padding: PaddingValues,
     onSelection: (Message) -> Unit,
+    onDelete: (Int) -> Unit
 ) {
     LazyColumn(contentPadding = padding) {
-        messageList(current = current, messages = messages, onSelection = { onSelection(it) })
+        messageList(
+            current = current,
+            messages = messages,
+            onSelection = { onSelection(it) },
+            onDelete = { onDelete(it) })
     }
 }
 
 private fun LazyListScope.messageList(
     current: Message?,
     messages: List<Message>,
-    onSelection: (Message) -> Unit
+    onSelection: (Message) -> Unit,
+    onDelete: (Int) -> Unit
 ) = items(
     count = messages.size,
     itemContent = { index ->
@@ -44,13 +51,26 @@ private fun LazyListScope.messageList(
             verticalAlignment = Alignment.CenterVertically
         ) {
             RadioButton(
+                modifier = Modifier.align(Alignment.Top),
                 selected = message == current,
                 onClick = { onSelection(message) }
             )
             Text(
+                modifier = Modifier.weight(1f),
                 text = message.message,
-                maxLines = 1,
                 overflow = TextOverflow.Ellipsis
+            )
+            IconButton(
+                modifier = Modifier.align(Alignment.Top).padding(end = 8.dp),
+                content = {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        imageVector = Icons.Default.Delete,
+                        tint = hexToColor("#F26549"),
+                        contentDescription = "Delete message"
+                    )
+                },
+                onClick = { onDelete(message.id) }
             )
         }
     }
